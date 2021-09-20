ff9ea5ef3e654ab721cfeadb4366b9f70ed2188a
Varun Kunchakuri <vakuncha@microsoft.com>
2021-09-20 12:55:17 +0530

    Adding Listing and deletion of elastic resource

--- Program.cs
+++ Program.cs
@@ -25,14 +25,25 @@ namespace ElasticSDKDemo
             string tenantId = Environment.GetEnvironmentVariable("AZURE_TENANT_ID");
             string clientId = Environment.GetEnvironmentVariable("AZURE_CLIENT_ID");
             string clientSecret = Environment.GetEnvironmentVariable("AZURE_CLIENT_SECRET");
+            string resourceGroup = "vakuncha-test-rg";
 
             var credentials = SdkContext.AzureCredentialsFactory
                                .FromServicePrincipal(clientId,clientSecret,tenantId,AzureEnvironment.AzureGlobalCloud);
 
 
             var microsoftElasticClient = new MicrosoftElasticClient(credentials);
-           
+
             microsoftElasticClient.SubscriptionId = subscriptionId;
+
+            var elasticResources = microsoftElasticClient.Monitors.ListByResourceGroup(resourceGroup);
+
+            if (elasticResources.Any())
+            {
+                var resourceToBeDeleted = elasticResources.First().Name;
+                Console.WriteLine("Deleting Resource:" + resourceToBeDeleted);
+                microsoftElasticClient.Monitors.Delete(resourceGroup,resourceToBeDeleted);
+            }
+           
             ElasticMonitorResource elasticMonitorResource= new ElasticMonitorResource();
 
             elasticMonitorResource.Location="eastus2euap";
@@ -40,19 +51,28 @@ namespace ElasticSDKDemo
             elasticMonitorResource.Properties = new MonitorProperties();
 
             elasticMonitorResource.Properties.UserInfo = new Microsoft.Azure.Management.Elastic.Models.UserInfo();
+            elasticMonitorResource.Properties.UserInfo.CompanyInfo = new CompanyInfo();
             elasticMonitorResource.Properties.UserInfo.FirstName = "varun";
             elasticMonitorResource.Properties.UserInfo.LastName = "kunchakuri";
             elasticMonitorResource.Properties.UserInfo.EmailAddress = "sdkdemo@mpliftrelastic20210901outlo.onmicrosoft.com";
+            elasticMonitorResource.Properties.UserInfo.CompanyInfo.Business = "cloud";
+            elasticMonitorResource.Properties.UserInfo.CompanyInfo.Country = "India";
+            elasticMonitorResource.Properties.UserInfo.CompanyInfo.Domain = "software";
+            elasticMonitorResource.Properties.UserInfo.CompanyInfo.State = "andhrapradesh";
+
 
             string resourceName = "csharpsdkCreate-" + new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
 
-            microsoftElasticClient.Monitors.Create("vakuncha-test-rg", resourceName, elasticMonitorResource);
+            Console.WriteLine("Creating Resource:" + resourceName);
+            microsoftElasticClient.Monitors.Create(resourceGroup, resourceName, elasticMonitorResource);
 
             MonitoringTagRulesProperties monitoringTagRulesProperties = new MonitoringTagRulesProperties();
             monitoringTagRulesProperties.LogRules = new LogRules();
             monitoringTagRulesProperties.LogRules.SendSubscriptionLogs = true;
             monitoringTagRulesProperties.LogRules.SendActivityLogs = true;
-            microsoftElasticClient.TagRules.CreateOrUpdate("vakuncha-test-rg",resourceName, "default",monitoringTagRulesProperties);
+            Console.WriteLine("Updating Tag rules for Resource:" + resourceName);
+            microsoftElasticClient.TagRules.CreateOrUpdate(resourceGroup,resourceName, "default",monitoringTagRulesProperties);
+    
         }
 
     }
